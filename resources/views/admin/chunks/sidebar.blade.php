73f981ade97bca86625677c7d005155964669e0e
ralf <ralf000@yandex.ru>
2017-03-22 17:06:51 +0300

    +главное меню

--- resources/views/admin/chunks/sidebar.blade.php
+++ resources/views/admin/chunks/sidebar.blade.php
@@ -1,14 +1,15 @@
 <div class="sidebar content-box" style="display: block;">
     <ul class="nav">
         <!-- Main menu -->
-        <li class="current"><a href="index.html"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
-        <li><a href="calendar.html"><i class="glyphicon glyphicon-calendar"></i> Calendar</a></li>
-        <li><a href="stats.html"><i class="glyphicon glyphicon-stats"></i> Statistics (Charts)</a></li>
-        <li><a href="tables.html"><i class="glyphicon glyphicon-list"></i> Tables</a></li>
-        <li><a href="buttons.html"><i class="glyphicon glyphicon-record"></i> Buttons</a></li>
-        <li><a href="editors.html"><i class="glyphicon glyphicon-pencil"></i> Editors</a></li>
-        <li><a href="forms.html"><i class="glyphicon glyphicon-tasks"></i> Forms</a></li>
-        <li class="submenu">
+        <li class="current"><a href="{{url('admin')}}"><i class="glyphicon glyphicon-home"></i> Главная</a></li>
+        <li><a href="{{url('admin/config')}}"><i class="glyphicon glyphicon-pencil"></i> Настройки</a></li>
+        <li><a href="{{url('admin/groups')}}"><i class="glyphicon glyphicon-tasks"></i> Группы каналов</a></li>
+        <li><a href="{{url('admin/changed-groups')}}"><i class="glyphicon glyphicon-tasks"></i> Измененные группы</a></li>
+        <li><a href="{{url('admin/exclude-groups')}}"><i class="glyphicon glyphicon-tasks"></i> Скрытые группы</a></li>
+        <li><a href="{{url('admin/changed-channels')}}"><i class="glyphicon glyphicon-list"></i> </i> Измененные каналы</a></li>
+        <li><a href="{{url('admin/exclude-channels')}}"><i class="glyphicon glyphicon-list"></i> Скрытые каналы</a></li>
+        <li><a href="{{url('admin/add-channels')}}"><i class="glyphicon glyphicon-list"></i> Добавленные каналы</a></li>
+        {{--<li class="submenu">
             <a href="#">
                 <i class="glyphicon glyphicon-list"></i> Pages
                 <span class="caret pull-right"></span>
@@ -18,6 +19,6 @@
                 <li><a href="login.html">Login</a></li>
                 <li><a href="signup.html">Signup</a></li>
             </ul>
-        </li>
+        </li>--}}
     </ul>
 </div>
