d936cd2563b73b4603bf13bfe473c2b5c26b97c6
ralf <ralf000@yandex.ru>
2017-03-24 10:01:53 +0300

    перевод страниц авторизации и регистрации

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
-    <title>{{ $title }}</title>
+    <title>{{ $title ?? '' }} |{{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
+                    {{--<ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
@@ -73,7 +73,7 @@
                                 </ul>
                             </li>
                         @endif
-                    </ul>
+                    </ul>--}}
                 </div>
             </div>
         </nav>
