aa3e4a7611a57abcf5123801bc707a3235d44dd1
ralf <ralf000@yandex.ru>
2017-04-06 12:54:16 +0300

    misc

--- config/main.php
+++ config/main.php
@@ -3,7 +3,7 @@
 return [
     'inputPlaylist' => [
         'label' => 'Ссылка на плейлист',
-        'value' => 'https://edem.tv/playlists/uplist/7087ffc69ceaa86cd41b0c6c54c3e20c/edem_pl.m3u8'
+        'value' => 'https://ссылка-на-плейлист.m3u8'
     ],
     'outputPlaylistName' => [
         'label' => 'Имя готового плейлиста ',
